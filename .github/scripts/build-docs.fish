34ffb363840eda452c38833cd5e2a74e5bb7ff92
esdmr <esdmr0@gmail.com>
2021-07-26 16:04:57 +0430

    Support backporting workflows

---
+++ .github/scripts/build-docs.fish
@@ -0,0 +1,12 @@
+# TODO: Build docs to build/docs
+
+# echo ::group::pnpm install
+# pnpm i
+# echo ::endgroup::
+# echo ::group::pnpm run build
+# pnpm run build
+# echo ::endgroup::
+# echo ::group::pnpm run build-docs
+# pnpm run build-docs
+# echo ::endgroup::
+exit
