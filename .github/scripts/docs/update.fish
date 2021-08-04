94066b81e1d9e9f76a3e0bf10e6f520ff6f0c12a
esdmr <esdmr0@gmail.com>
2021-08-04 18:57:17 +0430

    Fix pnpm cache for source

--- .github/scripts/docs/update.fish
+++ .github/scripts/docs/update.fish
@@ -1,31 +1,6 @@
 source (status dirname)/utils.fish
 
 begin
-    group Checkout
-    assert pushd master
-
-    # Checkout to the documentation branch
-    groupcmd git worktree add ../docs "$JOB_DOCS_BRANCH"
-    or fail Could not setup a git worktree for the documentation branch.
-
-    # Checkout to current branch
-    if test "$JOB_CURR_BRANCH" = master
-        echo Current branch is master, creating a symbolic link…
-        # Git worktrees must be unique. If the current branch is master, we must
-        # use a symbolic link instead. The current branch is assumed not to be
-        # the documentation branch.
-        groupcmd ln -vs (pwd) ../current
-        or fail Could not create a symbolic link for the current branch.
-    else
-        groupcmd git worktree add ../current "$JOB_CURR_BRANCH"
-        or fail Could not setup a git worktree for the current branch.
-    end
-
-    assert popd
-    endgroup
-end
-
-begin
     group Build
     assert env --chdir=current fish .github/scripts/docs/build.fish
     endgroup
