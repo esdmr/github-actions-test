b7b7d85e262f765297a675acd781e456f0154f84
esdmr <esdmr0@gmail.com>
2021-08-04 20:02:46 +0430

    Detach master and source

--- .github/scripts/docs/checkout.fish
+++ .github/scripts/docs/checkout.fish
@@ -4,43 +4,27 @@ begin
     group Checkout
     assert pushd master
 
+    # Checkout to current branch
+    groupcmd git worktree add --detach ../current "$JOB_CURR_BRANCH"
+    or fail Could not setup a git worktree for the current branch.
+
     # Checkout to the documentation branch
     groupcmd git worktree add ../docs "$JOB_DOCS_BRANCH"
     or fail Could not setup a git worktree for the documentation branch.
 
-    # Checkout to current branch
-    if test "$JOB_CURR_BRANCH" = master
-        echo Current branch is master, creating a symbolic link…
-        assert popd
-        # Git worktrees must be unique. If the current branch is master, we must
-        # use a symbolic link instead. The current branch is assumed not to be
-        # the documentation branch.
-        groupcmd ln -vs master current
-        or fail Could not create a symbolic link for the current branch.
-    else
-        groupcmd git worktree add ../current "$JOB_CURR_BRANCH"
-        or fail Could not setup a git worktree for the current branch.
-        assert popd
-    end
-
+    assert popd
     endgroup
 end
 
 begin
-    group DEBUG
-    assert groupcmd tar cvf debug.tar.xz *
+    group Sanity test
+    assert groupcmd test -f source/pnpm-lock.yaml
+    set -l pages_lockfiles master/.github/pages-template/**/pnpm-lock.yaml
+    assert groupcmd test (count $pages_lockfiles) -gt 0
+
+    for file in pages_lockfiles
+        assert groupcmd test -f $file
+    end
+
     endgroup
 end
-
-# begin
-#     group Sanity test
-#     assert groupcmd test -f source/pnpm-lock.yaml
-#     set -l pages_lockfiles master/.github/pages-template/**/pnpm-lock.yaml
-#     assert groupcmd test (count $pages_lockfiles) -gt 0
-
-#     for file in pages_lockfiles
-#         assert groupcmd test -f $file
-#     end
-
-#     endgroup
-# end
