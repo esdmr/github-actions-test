8f48235c545ffccdbbdefe3de1d56c5a7de1ade1
esdmr <esdmr0@gmail.com>
2021-08-08 23:14:18 +0430

    Fix git worktree

--- .github/scripts/docs/checkout.fish
+++ .github/scripts/docs/checkout.fish
@@ -5,11 +5,13 @@ begin
     assert pushd master
 
     # Checkout to current branch
-    groupcmd git worktree add --detach ../current "$JOB_CURR_BRANCH"
+    groupcmd git worktree add --detach -b "$JOB_CURR_BRANCH" ../current \
+        "origin/$JOB_CURR_BRANCH"
     or fail Could not setup a git worktree for the current branch.
 
     # Checkout to the documentation branch
-    groupcmd git worktree add ../docs "$JOB_DOCS_BRANCH"
+    groupcmd git worktree add -b "$JOB_DOCS_BRANCH" ../docs \
+        "origin/$JOB_DOCS_BRANCH"
     or fail Could not setup a git worktree for the documentation branch.
 
     assert popd
