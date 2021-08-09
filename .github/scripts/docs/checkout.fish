e879abb66398abcdc67779c9716af128a383717c
esdmr <esdmr0@gmail.com>
2021-08-09 19:10:18 +0430

    Correctly case Git

--- .github/scripts/docs/checkout.fish
+++ .github/scripts/docs/checkout.fish
@@ -6,12 +6,12 @@ begin
 
     # Checkout to current branch
     groupcmd git worktree add --detach ../current "origin/$JOB_CURR_BRANCH"
-    or fail Could not setup a git worktree for the current branch.
+    or fail Could not setup a Git worktree for the current branch.
 
     # Checkout to the documentation branch
     groupcmd git worktree add -b "$JOB_DOCS_BRANCH" ../docs \
         "origin/$JOB_DOCS_BRANCH"
-    or fail Could not setup a git worktree for the documentation branch.
+    or fail Could not setup a Git worktree for the documentation branch.
 
     assert popd
     endgroup
